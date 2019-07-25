0df78f751a4a8cbf365a6d20044b2aa97814c21e
lhorvce <lhorvce@hotmail.com>
2019-07-25 17:43:37 +0800

    add py file

--- run.sh
+++ run.sh
@@ -13,6 +13,6 @@ curl -XPUT "$1/geojson" -H 'Content-Type: application/json' -d'
   }
 }'
 
-for ((i=1; i<=5; i++)); do
+for ((i=1; i<=6; i++)); do
     curl -H "Content-Type: application/x-ndjson" -XPOST "$1/geojson/_bulk?pretty" --data-binary @part$i.json
 done
